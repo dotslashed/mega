f28ec3cabdba3eb2a3ec1b2740ee36de20786a52
Bhargab Kaushik <73475291+dotslashed@users.noreply.github.com>
2021-11-14 17:07:55 +0530

    Update kr.sh

--- kr.sh
+++ kr.sh
@@ -7,7 +7,7 @@ echo "Installing..."
 wget "https://github.com/assetnote/kiterunner/releases/download/v1.0.2/kiterunner_1.0.2_linux_amd64.tar.gz"
 
 
-tar -xvzf kiterunner_1.0.2_linux_amd64
+tar -xvzf kiterunner_1.0.2_linux_amd64.tar.gz
 
 sudo mv kr /usr/local/bin
 
